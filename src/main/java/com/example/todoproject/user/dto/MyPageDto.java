f48c3325313c4e63e3448b97f36290484bd12ea0
Taaate <an981022@naver.com>
2024-03-18 13:33:06 +0900

    feat: 마이 페이지 구현

---
+++ src/main/java/com/example/todoproject/user/dto/MyPageDto.java
@@ -0,0 +1,7 @@
+package com.example.todoproject.user.dto;
+
+public record MyPageDto(
+        String name,
+        String email
+) {
+}
--- src/main/java/com/example/todoproject/user/service/UserService.java
+++ src/main/java/com/example/todoproject/user/service/UserService.java
@@ -6,27 +6,41 @@ import com.example.todoproject.redis.RefreshToken;
 import com.example.todoproject.redis.RefreshTokenRepository;
 import com.example.todoproject.todo.dto.RefreshTokenDto;
 import com.example.todoproject.user.domain.User;
+import com.example.todoproject.user.dto.MyPageDto;
 import com.example.todoproject.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserService {
 
     private final JwtService jwtService;
+    private final UserRepository userRepository;
     private final RefreshTokenRepository refreshTokenRepository;
 
+    @Transactional
     public TokenResponse issueToken(String token) {
         String email = jwtService.extractEmail(token);
         return jwtService.toTokenResponse(email);
     }
 
+    @Transactional
     public TokenResponse reIssueToken(RefreshTokenDto refreshTokenDto) {
         RefreshToken refreshToken = refreshTokenRepository.findByToken(refreshTokenDto.value())
                 .orElseThrow(() -> new IllegalArgumentException("2번 후보"));
         return jwtService.toTokenResponse(refreshToken.getEmail());
+    }
+
+    public MyPageDto getMyPage(String email) {
+        User user = getUser(email);
+        return new MyPageDto(user.getName(), user.getEmail());
+    }
+
+    private User getUser(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저를 찾을 수 없습니다."));
     }
 }
