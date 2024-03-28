20a25ab67af9ad7151045c27f754b0e76fc019ca
Taaate <an981022@naver.com>
2024-03-28 13:38:42 +0900

    fix: 레디스 정보 수정

--- src/main/java/com/example/todoproject/user/domain/User.java
+++ src/main/java/com/example/todoproject/user/domain/User.java
@@ -1,12 +1,15 @@
 package com.example.todoproject.user.domain;
 
+//import com.example.todoproject.team.domain.Team;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -37,4 +40,14 @@ public class User {
     @Column(name = "is_deleted")
     private boolean deleted;
 
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private Team team;
+//
+//    public void addTeam(Team team) {
+//        this.team = team;
+//    }
+//
+//    public void exit(Team team) {
+//
+//    }
 }
