168256cef3e63754fb6bd4ac767bfeb66977ca58
An Tae ho <an981022@naver.com>
2024-03-28 13:59:51 +0900

    fix: 레디스 정보 수정 (#5)
    * fix: 레디스 정보 수정
    
    * fix: 레디스 정보 수정

--- src/main/java/com/example/todoproject/config/RedisConfig.java
+++ src/main/java/com/example/todoproject/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.example.todoproject.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -8,8 +9,14 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.port}")
+    private int port;
+
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("127.0.0.1", 6379);
+        return new LettuceConnectionFactory(host, port);
     }
 }
