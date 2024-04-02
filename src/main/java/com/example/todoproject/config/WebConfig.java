480b7ddd93051480af613ea367328c75851d4b42
Taaate <an981022@naver.com>
2024-04-02 20:53:46 +0900

    hotfix: cors 에러 해결

---
+++ src/main/java/com/example/todoproject/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.example.todoproject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:8081",
+                        "http://3.36.153.201",
+                        "http://3.36.153.201:8081"
+                        )
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
+                .exposedHeaders("Authorization", "RefreshToken");
+    }
+}
