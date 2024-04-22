fc2e609ea048a4ae5b61b50fa364d17eef81234b
Taaate <an981022@naver.com>
2024-04-22 15:35:42 +0900

    feat: 비동기 처리 및 쓰레드 설정 추가

---
+++ src/main/java/com/example/todoproject/config/AsyncConfig.java
@@ -0,0 +1,33 @@
+package com.example.todoproject.config;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    @Bean(name = "asyncExecutor")
+    public Executor getAsyncExecutor() {
+        return new ThreadPoolExecutor(
+                3,
+                3,
+                30,
+                TimeUnit.SECONDS,
+                new SynchronousQueue<>()
+        );
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return AsyncConfigurer.super.getAsyncUncaughtExceptionHandler();
+    }
+}
