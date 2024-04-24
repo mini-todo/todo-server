957aead87b565aa45cc641914d43a4ad7021c8ac
Taaate <an981022@naver.com>
2024-04-25 08:25:38 +0900

    fix: cors 설정

--- src/main/java/com/example/todoproject/config/WebConfig.java
+++ src/main/java/com/example/todoproject/config/WebConfig.java
@@ -16,6 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://localhost:9090",
                         "http://3.36.153.201",
                         "http://3.36.153.201:8081",
+                        "http://3.36.153.201:80",
                         "http://3.36.153.201:9090"
                         )
                 .allowedHeaders("*")
