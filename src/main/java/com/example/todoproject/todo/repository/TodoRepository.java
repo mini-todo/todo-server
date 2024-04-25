d2d12308ad5d1d1b798a1e286e81bc8514654f03
Taaate <an981022@naver.com>
2024-04-25 13:23:34 +0900

    fix: 투두 조회 설정

--- src/main/java/com/example/todoproject/todo/repository/TodoRepository.java
+++ src/main/java/com/example/todoproject/todo/repository/TodoRepository.java
@@ -1,12 +1,20 @@
 package com.example.todoproject.todo.repository;
 
 import com.example.todoproject.todo.domain.Todo;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    List<Todo> findAllByUserId(Long userId);
     Optional<Todo> findByUserIdAndId(Long userId, Long id);
+
+    @Query("select t from Todo t where t.userId = :userId and MONTH(:date) = MONTH(t.date)")
+    List<Todo> findAllMonthlyByUserIdAndAndDate(@Param("userId") Long userId,@Param("date") LocalDate date);
+
+    @Query("select t from Todo t where t.userId = :userId and DATE(:date) = DATE(t.date)")
+    List<Todo> findAllDailyByUserIdAndAndDate(@Param("userId") Long userId,@Param("date") LocalDate date);
 }
