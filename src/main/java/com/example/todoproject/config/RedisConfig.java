64cd9505dccd3e4fadb0aaa430dd16e892a007fe
Taaate <an981022@naver.com>
2024-06-06 13:53:43 +0900

    임시

--- src/main/java/com/example/todoproject/config/RedisConfig.java
+++ src/main/java/com/example/todoproject/config/RedisConfig.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
+@EnableRedisRepositories(basePackages = "com.example.todoproject.redis")
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
